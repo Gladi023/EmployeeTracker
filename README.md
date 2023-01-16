73d0ed340bd7cb18496e310f3489b2fc894aaeb9
Gladi023 <Gladimar.Villamil@gmail.com>
2023-01-16 13:21:37 -0800

    index.js and readme edited

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # EmployeeTracker
+# Table-of-Contents
+
+  * [UserStory](#userstory)
+  * [Acceptance Criteria](#acceptance-criteria)
+  * [Git Repo](#git-repo)
+  * [Walk through video] (#video)
+  * [credits] (#credits)
+
+
+## [User Story](#table-of-contents)
+
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+
+## [Acceptance Criteria](#table-of-contents)
+GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+
+
+## [Git Repo](#table-of-contents)
+https://github.com/Gladi023/EmployeeTracker
+## Tools Used
+
+  * sequelize
+  * MYSQL2
+
+
+  
+
+## [Walk through video](#video)
+
+Here is the link to the walkthrough video:
+https://vimeo.com/
+
+## [credits](#credits)
+Educational Modules from Trilogy Education Services and Columbia University were used to create this app.
--- index.js
+++ index.js
@@ -126,3 +126,148 @@ async function viewAllEmployees() {
 firstPrompt();
 }
 
+// GET DEPARTMENT
+const getDept = async (deptChoices) => {
+    const { department } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'department',
+        message: 'Departments: ',
+        choices: deptChoices
+      }
+    ]);
+    try {
+        const query = `SELECT 
+                            employee.id, 
+                            employee.first_name, 
+                            employee.last_name, 
+                            role.title, 
+                            department.name
+                        FROM employee
+                        JOIN role
+                            ON employee.role_id = role.id
+                        JOIN department
+                            ON department.id = role.department_id
+                        WHERE department.id = ?`;
+        const results = await connection.query(query, department);
+        console.table(results);
+        firstPrompt();
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+//create employee
+
+async function createEmployee() {
+    try {
+      const roles = await connection.query(`SELECT id, title, salary FROM role`);
+      const roleChoices = roles.map(({ id, title, salary }) => ({ value: id, title: `${title} (${salary})` }));
+      const { firstName, lastName, roleId } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "firstName",
+          message: "Employee First Name: "
+        },
+        {
+          type: "input",
+          name: "lastName",
+          message: "Employee Last Name: "
+        },
+        {
+          type: "list",
+          name: "roleId",
+          message: "Employee Role: ",
+          choices: roleChoices
+        }
+      ]);
+      await connection.query(`INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)`, [firstName, lastName, roleId]);
+console.log("Employee added successfully.");
+firstPrompt();
+} catch (err) {
+    throw err;
+  }
+};
+  
+// REMOVE EMPL
+async function removeEmployee() {
+    try {
+      const employees = await connection.query(`SELECT id, first_name, last_name FROM employee`);
+      const employeeChoices = employees.map(({ id, first_name, last_name }) => ({ value: id, name: `${first_name} ${last_name}` }));
+      const { employee } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "employee",
+          message: "Employee To Be Deleted: ",
+          choices: employeeChoices
+        }
+      ]);
+      await connection.query(`DELETE FROM employee WHERE id = ?`, [employee]);
+      console.log("Employee removed successfully.");
+      firstPrompt();
+    } catch (err) {
+      throw err;
+    }
+  }
+  //UPDATE EMPLOYEE ROLE
+
+  async function updateEmployeeRole() {
+    try {
+      const employees = await connection.query(`SELECT id, first_name, last_name FROM employee`);
+      const employeeChoices = employees.map(({ id, first_name, last_name }) => ({ value: id, name: `${first_name} ${last_name}` }));
+      const roles = await connection.query(`SELECT id, title, salary FROM role`);
+      const roleChoices = roles.map(({ id, title, salary }) => ({ value: id, title: `${title} (${salary})` }));
+      const { employeeId, roleId } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Select an employee: ",
+          choices: employeeChoices
+        },
+        {
+          type: "list",
+          name: "roleId",
+          message: "Select a new role for the employee: ",
+          choices: roleChoices
+        }
+      ]);
+      await connection.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [roleId, employeeId]);
+      console.log("Employee role updated successfully.");
+      firstPrompt();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  //ADD ROLE
+  async function addToRole(department) {
+    try {
+      const { title, salary, department } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "Role title: "
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Role Salary: "
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Department: ",
+          choices: department
+        },
+      ]);
+      await connection.query(
+        "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+        [title, salary, department]
+      );
+      console.log("Role added successfully.");
+      firstPrompt();
+    } catch (err) {
+      throw err;
+    }
+  }
+  
