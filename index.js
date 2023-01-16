4b7570b5909d9d78e0d8f608d0e68a2e0f861b94
Gladi023 <Gladimar.Villamil@gmail.com>
2023-01-16 12:56:19 -0800

    employee view func added

--- index.js
+++ index.js
@@ -75,3 +75,54 @@ const choices = [
       });
   }
   
+//view all employees
+async function viewAllEmployees() {
+    try {
+        const query = 
+        `SELECT 
+            employee.id, 
+            employee.first_name, 
+            employee.last_name, 
+            role.title, 
+            department.name AS department, 
+            role.salary, 
+            CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM employee
+        LEFT JOIN role
+            ON employee.role_id = role.id
+        LEFT JOIN department
+            ON department.id = role.department_id
+        LEFT JOIN employee manager
+            ON manager.id = employee.manager_id`
+
+        const result = await connection.query(query);
+        console.table(result);
+    } catch (err) {
+        throw err;
+    }
+    firstPrompt();
+}
+
+//VIEW EMPLOYEES BY DEPARTMENT
+   async function viewEmployeesByDepartment(){
+    try{ 
+    const query =
+    `SELECT 
+        department.id, 
+        department.name, 
+        role.salary
+    FROM employee
+    LEFT JOIN role 
+        ON employee.role_id = role.id
+    LEFT JOIN department
+        ON department.id = role.department_id
+    GROUP BY department.id, department.name, role.salary`;
+  
+    const result = await connection.query(query);
+    console.table(result);
+} catch (err) {
+    throw err;
+}
+firstPrompt();
+}
+
